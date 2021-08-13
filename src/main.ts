e9932b06fddfb89fe3084878755235743a836e36
Cyb3r Jak3 <jake@jwhite.network>
2021-08-13 16:47:10 -0400

    Fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
-import * as core from "@actions/core";
-import * as exec from "@actions/exec";
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 
 export async function run(): Promise<void> {
-  core.startGroup("Installing HTML5Validator");
-  await exec.exec("pip", [
-    "install",
-    "-disable-pip-version-check",
-    "--no-cache-dir ",
-    "--upgrade",
-    "html5validator",
-  ]);
-  core.endGroup();
+  core.startGroup('Installing HTML5Validator')
+  await exec.exec('pip', [
+    'install',
+    '-disable-pip-version-check',
+    '--no-cache-dir ',
+    '--upgrade',
+    'html5validator'
+  ])
+  core.endGroup()
 }
 
-run();
+run()
