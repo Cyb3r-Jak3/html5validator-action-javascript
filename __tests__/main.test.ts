bddfbc1d688b397feb225172e3fe174a32c4e39f
Cyb3r Jak3 <jake@jwhite.network>
2021-09-18 11:40:17 -0400

    Trying with docker bake for tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,30 +7,33 @@ describe('Main', () => {
     await run()
     assertWriteCalls([`::error::Need either root or config set${os.EOL}`])
   });
+
   it('custom validator version', async () => {
     // jest.setTimeout(10000000);
     generateNewConfig()
     setInput('validator_version', '0.4.0')
     await run()
   });
+
   it('invalid files', async () => {
     // jest.setTimeout(10000000);
     setInput('root', 'tests/invalid/')
     setInput('log_level', 'DEBUG')
     await run()
-})
+  });
+});
 
 function generateNewConfig(): void {
   setInput('root', 'tests/valid/')
   setInput('log_level', 'DEBUG')
   setInput('format', 'json')
   setInput('css', 'true')
-}
+};
 
 // See: https://github.com/actions/toolkit/blob/a1b068ec31a042ff1e10a522d8fdf0b8869d53ca/packages/core/src/core.ts#L89
 function getInputName(name: string): string {
   return `INPUT_${name.replace(/ /g, '_').toUpperCase()}`
-}
+};
 
 function setInput(name: string, value: string): void {
   process.env[getInputName(name)] = value
@@ -42,4 +45,4 @@ function assertWriteCalls(calls: string[]): void {
   for (let i = 0; i < calls.length; i++) {
     expect(process.stdout.write).toHaveBeenNthCalledWith(i + 1, calls[i])
   }
-}
+};
